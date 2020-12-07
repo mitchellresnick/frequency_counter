65629754a5f746d26deab1970016baaae2a04557
Mitchell Resnick <mitchellresnick@gmail.com>
2020-12-06 23:45:01 -0500

    Update to handle xlsx files, multiple sheets

--- frequency_count.py
+++ frequency_count.py
@@ -1,29 +1,128 @@
 import csv
+import pathlib
+import sys
+from os import path, walk, execv
+from pandas import read_excel, DataFrame, ExcelWriter
 
-# ######################
-# ALLISON EDIT SECTION
+def get_user_input(prompt, storage_variable, list: list, display_help: bool):
+    i = 0
+    print()
+    for item in list:
+        print(i, ": %s" % item)
+        i += 1
+    print()
+    storage_variable = input(prompt)
+    if storage_variable.lower() == 'help' and display_help:
+        print("If you do not see the file that you want to analyze, please ensure that it is in the same directory as %s\n\n---------" % path.basename(__file__))
+    else:
+        try:
+            return int(storage_variable)
+        except ValueError:
+            if display_help:
+                print("\n!!!!! \nYou did not enter an allowed value. Please enter a numeral or 'help'.")
+            else:
+                print("You did not enter an allowed value. Please enter a numeral.")
+            return None
 
-# The word in the header to exclude from analysis
-headerKeyword = "Keywords"
-# The column to analyze, this is zero indexed. Column A = 0, column B = 1, etc.
-columnNum = 3
-# #####################################
+def list_columns(workbook: str, worksheet: int):
+    df = read_excel(workbook, sheet_name=None)
 
-wordFrequency = {}
+    for value in df[worksheet]:
+        print("Columns:", *value.columns, "\n", sep=", ")
+
+def list_worksheets(workbook: str):
+    df = read_excel(workbook, sheet_name=None)
+
+    return df.keys()
+
+def main():
+    # Gets the directory of this python file
+    dir = pathlib.Path(__file__).parent.absolute()
+    print(dir)
+
+    # Gets all .xlsx files in the directory
+    f = []
+    for (firpath, dirnames, filenames) in walk(dir):
+        for name in filenames:
+            if name.endswith(".xlsx") and not name.endswith("_result.xlsx"):
+                f.append(name)
+
+    # Get user input
+    file_index = None
+    if len(f) == 1:
+        print("Only one .xlsx file has been identified. Automatically starting analysis of %s" % f[0])
+        file_index = 0
+    else:
+        user_prompt = "Please enter the number of the file from the list above that you would like to analyze (Enter 'help' for help): "
+        while file_index is None:
+            file_index = get_user_input(user_prompt, file_index, f, True)
 
 
-with open("C:/Users/atres/OneDrive/Desktop/Prospectus/Prospectus Writing/Book_10_Sample.csv", "r") as f:
-    csvreader = csv.reader(f)
-    for row in csvreader:
-        if row[columnNum] != headerKeyword:
-            for word in row[columnNum].split(' '):
+    # Open file for reading
+    workbook = None
+    try:
+        workbook = open(path.join(dir, f[file_index]), 'r')
+    except PermissionError:
+        print("\n\n!!!!!!!!!\nUnable to access this file. Perhaps you already have it open? If so, close it before trying again.\n")
+        sys.exit()
+    # Print file contents
+    list_worksheets(str(workbook.name))
+
+    worksheet_column_map = {}
+    df = read_excel(str(workbook.name), sheet_name=None)
+
+    column_question = "Which column from the list above should be analyzed in "
+    user_input = None
+    for (key, value) in df.items():
+        while user_input is None:
+            user_input = get_user_input(column_question + key + "?: ", user_input, value, False)
+        worksheet_column_map[key] = user_input
+        user_input = None
+
+    worksheet_frequency_holder = {}
+    worksheet_frequency = {}
+    wordFrequency = {}
+    wordFrequencyDF = DataFrame()
+
+    
+
+    # output = open(path.join(dir, f[file_index])[:-5] + "_result.xlsx", 'w')
+    for (key, value) in worksheet_column_map.items():
+        wordFrequency.clear()
+        wordFrequencyDF = DataFrame()
+        # print(key, "\n--------")
+        column_name = df[key].columns[value]
+        for row in df[key][column_name].values:
+            for word in row.split(' '):
                 try:
                     wordFrequency[word] += 1
                 except KeyError:
                     wordFrequency[word] = 1
+        dataframe_append = DataFrame.from_dict(wordFrequency, orient='index')
+        wordFrequencyDF = wordFrequencyDF.append(dataframe_append)
+        with ExcelWriter(f[file_index][:-5] + "_result.xlsx", engine='openpyxl', mode='a') as writer:
+            wordFrequencyDF.to_excel(writer, sheet_name=key, header=False, startrow=0, startcol=0)
+    
+    # output.close()
+    workbook.close()
+    
+    # # Have user identify what worksheet to analyze
+    # worksheet_input = None
+    # worksheets = list_worksheets(str(workbook.name))
+    # user_prompt = "Which worksheet would you like to analyze from the list above?: "
+    # worksheet_input = get_user_input(user_prompt, worksheet_input, worksheets, False)
 
-with open("C:/Users/atres/OneDrive/Desktop/Prospectus/Prospectus Writing/Book_10_Sample_Results.csv", "w", newline='', encoding='utf-8') as o:
-    csvwriter = csv.writer(o)
-    csvwriter.writerow(["Keyword", "Count"])
-    for (key, value) in wordFrequency.items():
-        csvwriter.writerow([key, value])
+    # # Have user identify what column to analyze
+    # column_input = None
+    # column = list_columns(str(workbook.name), worksheets[worksheet_input])
+    # user_prompt = "Which column would you like to analyze from the list above?: "
+    # worksheet_input = get_user_input(user_prompt, worksheet_input, worksheets, False)
+
+    # Get header row from the file
+    # analysis_column = None
+    # column_prompt = "Please select the number of the column that you would like to analyze. Column headings are shown above."
+    # while analysis_column is None:
+    #     get_user_input(column_prompt, analysis_column, columns, False)
+
+if __name__ == "__main__":
+    main()
